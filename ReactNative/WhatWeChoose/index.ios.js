b69de3bc5d5f48e6a3673ec3834d07f1e05aa802
mobileyangli <mobile_yangli@163.com>
2016-06-13 16:20:16 +0800

    make first commit

---
+++ ReactNative/WhatWeChoose/index.ios.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+'use strict';
+
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Image,
+    Text,
+    View,
+    NavigatorIOS,
+} from 'react-native';
+
+
+/**
+ * 1、添加HelloWorld Label
+ */
+// class WhatWeChoose extends Component {
+//     render() {
+//         return (
+//             <Text style={styles.text}>Hello world!</Text>
+//         );
+//     }
+// }
+
+/**
+ * 2、添加原生控件NavigationBar
+ */
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <Text style={styles.text}>Hello world!</Text>
+        );
+    }
+}
+
+class WhatWeChoose extends Component {
+    render() {
+        return (
+            <NavigatorIOS style={styles.container} initialRoute={{title:'WhatWeChoose', component:HelloWorld}}/>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    text     : {
+        color          : 'black',
+        backgroundColor: 'white',
+        fontSize       : 30,
+        margin         : 80
+    }
+});
+
+AppRegistry.registerComponent('WhatWeChoose', ()=>WhatWeChoose);
