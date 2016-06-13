1e5414e284f6074558c4ba75bf1df23ef9124d77
mobileyangli <mobile_yangli@163.com>
2016-06-13 17:13:40 +0800

    setup.js script file can work, it can init something in process

--- ReactNative/WhatWeChoose/index.ios.js
+++ ReactNative/WhatWeChoose/index.ios.js
@@ -6,58 +6,7 @@
 
 'use strict';
 
-import React, {Component} from 'react';
-import {
-    AppRegistry,
-    StyleSheet,
-    Image,
-    Text,
-    View,
-    NavigatorIOS,
-} from 'react-native';
+import {AppRegistry} from'react-native';
+import setup from './js/setup';
 
-
-/**
- * 1、添加HelloWorld Label
- */
-// class WhatWeChoose extends Component {
-//     render() {
-//         return (
-//             <Text style={styles.text}>Hello world!</Text>
-//         );
-//     }
-// }
-
-/**
- * 2、添加原生控件NavigationBar
- */
-
-class HelloWorld extends Component {
-    render() {
-        return (
-            <Text style={styles.text}>Hello world!</Text>
-        );
-    }
-}
-
-class WhatWeChoose extends Component {
-    render() {
-        return (
-            <NavigatorIOS style={styles.container} initialRoute={{title:'WhatWeChoose', component:HelloWorld}}/>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    text     : {
-        color          : 'black',
-        backgroundColor: 'white',
-        fontSize       : 30,
-        margin         : 80
-    }
-});
-
-AppRegistry.registerComponent('WhatWeChoose', ()=>WhatWeChoose);
+AppRegistry.registerComponent('WhatWeChoose', setup);
---
+++ ReactNative/WhatWeChoose/js/WhatWeChoose.js
@@ -0,0 +1,72 @@
+/**
+ * Copyright YangDongLing (Link Yang)
+ *
+ * @flow
+ *
+ */
+
+'use strict';
+
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Image,
+    Text,
+    View,
+    NavigatorIOS,
+} from 'react-native';
+
+
+/**
+ * 1、添加HelloWorld Label
+ */
+// class WhatWeChoose extends Component {
+//     render() {
+//         return (
+//             <Text style={styles.text}>Hello world!</Text>
+//         );
+//     }
+// }
+
+/**
+ * 2、添加原生控件NavigationBar
+ */
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <Text style={styles.text}>Hello world!</Text>
+        );
+    }
+}
+
+var WhatWeChoose = React.createClass({
+    render(){
+        return (
+            <WhatWeChooseNavigator />
+        );
+    }
+});
+
+class WhatWeChooseNavigator extends Component {
+    render() {
+        return (
+            <NavigatorIOS style={styles.container} initialRoute={{title:'WhatWeChoose', component:HelloWorld}}/>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    text     : {
+        color          : 'black',
+        backgroundColor: 'white',
+        fontSize       : 30,
+        margin         : 80
+    }
+});
+
+module.exports = WhatWeChoose;
